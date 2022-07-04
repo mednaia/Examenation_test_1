906f39ca9db1ebba8422934894c5ea33048716c6
Ekaterina <tiurina.kate@gmail.com>
2022-07-04 13:30:26 +0300

    Creation array of strings with using methods

--- Test_1/Program.cs
+++ Test_1/Program.cs
@@ -2,35 +2,35 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-//Creation of input array
-Console.Write("Enter number of strings: ");
-int lengthInputArray=int.Parse(Console.ReadLine()??"0");
-string[] inputArray = new string[lengthInputArray];
-for (int i=0;i<inputArray.Length;i++)
-    {
-        Console.WriteLine("Enter your strings: ");
-        inputArray[i]=Console.ReadLine()??"0";
-    }
-Console.WriteLine();
-Console.WriteLine("Input array:");
-for (int i=0;i<inputArray.Length;i++)
-    Console.Write($" '{inputArray[i]}' ");
 
-//Searching strings, which contain three or less chars
-int outputLengthArray = 0;
-for (int i = 0; i < inputArray.Length; i++)
-    if (inputArray[i].Length <= 3) outputLengthArray++;
-string[] outputArray = new string[outputLengthArray];
-int j = 0;
-for (int i = 0; i < inputArray.Length; i++)
+string[]CreateArrayOfStrings(int count)
 {
-    if (inputArray[i].Length <= 3)
-    {
-        outputArray[j] = inputArray[i];
-        j++;
-    }
+    return new string[count];
 }
+
+void FillArrayOfStrings(string[] stringArr)
+{
+    for (int i=0;i<stringArr.Length;i++)
+    {
+        stringArr[i]=Console.ReadLine()??"0";
+    }   
+}
+
+void PrintArrayOfStrings(string[] arrayOfStrins)
+{
+    for (int i=0;i<arrayOfStrins.Length;i++)
+    Console.Write($" '{arrayOfStrins[i]}' ");
+}
+
+Console.Clear();
+Console.Write("Enter your number of strings: ");
+int lengthInputArray=int.Parse(Console.ReadLine()??"0");
+string[]inputArray=CreateArrayOfStrings(lengthInputArray);
+Console.WriteLine("Enter your strings: ");
+FillArrayOfStrings(inputArray);
+Console.WriteLine($"Input array: ");
+PrintArrayOfStrings(inputArray);
 Console.WriteLine();
-Console.WriteLine("Output array, which strings contain three or less chars:");
-for (int number=0; number<outputArray.Length; number++)
-    Console.Write($" '{outputArray[number]}' ");
+
+
+

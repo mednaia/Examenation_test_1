6917dce7aee70d12caee069a59cce8deabd7a89a
Ekaterina <tiurina.kate@gmail.com>
2022-07-04 20:06:59 +0300

    Complete solution

--- Test_1/Program.cs
+++ Test_1/Program.cs
@@ -12,6 +12,7 @@ void FillArrayOfStrings(string[] stringArr)
 {
     for (int i=0;i<stringArr.Length;i++)
     {
+        Console.WriteLine($"Enter string number {i+1}: ");
         stringArr[i]=Console.ReadLine()??"0";
     }   
 }
@@ -22,15 +23,51 @@ void PrintArrayOfStrings(string[] arrayOfStrins)
     Console.Write($" '{arrayOfStrins[i]}' ");
 }
 
+int DefineLengthOfOutputArray(string[] arrayStr)
+{
+int number=0;
+for(int index=0;index<arrayStr.Length;index++)
+    if (arrayStr[index].Length<=3)
+        number++;
+return number;
+}
+
+void FillOutputArray(string[] arrInput, string[] arrOutput)
+{
+int j = 0;
+    for (int i = 0; i < arrInput.Length; i++)
+    {
+        if (arrInput[i].Length <= 3)
+        {
+            arrOutput[j] = arrInput[i];
+            j++;
+        }
+    }
+}
+
+
 Console.Clear();
 Console.Write("Enter your number of strings: ");
 int lengthInputArray=int.Parse(Console.ReadLine()??"0");
-string[]inputArray=CreateArrayOfStrings(lengthInputArray);
-Console.WriteLine("Enter your strings: ");
+string[] inputArray=CreateArrayOfStrings(lengthInputArray);
 FillArrayOfStrings(inputArray);
 Console.WriteLine($"Input array: ");
 PrintArrayOfStrings(inputArray);
 Console.WriteLine();
+int lengthOfOutputArray=DefineLengthOfOutputArray(inputArray);
+if (lengthOfOutputArray>0)
+{
+    string[] outputArray=CreateArrayOfStrings(lengthOfOutputArray);
+    FillOutputArray(inputArray,outputArray);
+    Console.WriteLine("Output array, which strings contain three or less chars:");
+    PrintArrayOfStrings(outputArray);    
+}
+else
+{
+    Console.WriteLine("Input array contain strings, which length more, than three chars.");
+}
+
+
 
 
 
